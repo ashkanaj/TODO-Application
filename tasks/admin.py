4346b281b493f687155baf78f87c9d1edd075bef
Ashkan Jafarinejad <ashkanaj2019@gmail.com>
2025-01-27 16:01:54 +0330

    ToDo-List Application

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'due_date', 'completed','created_at', 'updated_at')
+    list_filter = ('completed', 'due_date')
+    search_fields = ('title','description')
